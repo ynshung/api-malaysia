bf9602d6adfff24952d69c42312dd411a6ae5129
Young Shung <ynshung3@gmail.com>
2020-07-06 12:49:34 +0800

    Initial commit

---
+++ APIMS-web.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from urllib import request
+from pathlib import Path
+import re
+
+df2 = pd.DataFrame()
+
+date = pd.to_datetime('2018/01/01 2300', format='%Y/%m/%d %H%M')
+
+def returnFile(url=False):
+    if url:
+        return date.strftime(format='%Y/%m/%d/%H%M') + '.json'
+    else:
+        return date.strftime(format='%Y-%m-%d-%H%M') + '.json'
+
+def returnURL():
+    return 'http://apims.doe.gov.my/data/public_v2/CAQM/hours24/' + returnFile(True)
+
+def getJSON():
+    fpath = 'cache/' + returnFile()
+    cache_file = Path(fpath)
+    if not cache_file.is_file():
+        print("Requesting " + returnURL())
+        request.urlretrieve(returnURL(),fpath)
+    return fpath
+
+def clean(x):
+    try:
+        return int(re.sub('\D', '', str(x)))
+    except:
+        return np.nan
+
+while date <= pd.to_datetime('now'):
+    print('Processing ' + returnFile())
+    try:
+        json = pd.read_json(getJSON())
+    except:
+        print('Error occured')
+        date += pd.DateOffset(hours=8)
+        continue
+
+    try:
+        df = json['24hour_api'].apply(pd.Series)
+    except:
+        df = json['24hour_api_apims'].apply(pd.Series)
+
+    new_header = df.iloc[0]
+    df = df[1:]
+    df.columns = new_header
+
+    df = df.set_index('Location')
+    df = df.drop(columns='State',axis=1)
+    df = df.applymap(clean)
+
+    df = df.transpose()
+    df = df.astype(np.float16)
+
+    ls = list(df.index)
+    passed = False
+    for i,v in enumerate(ls):
+        if v == '12:00AM':
+            passed = True
+        if passed:
+            ls[i] = date.strftime(format='%Y/%m/%d') + ' ' + ls[i]
+        else:
+            ls[i] = (date-pd.DateOffset(1)).strftime(format='%Y/%m/%d') + ' ' + ls[i]
+    df.index = pd.to_datetime(ls)
+
+    df2 = df2.combine_first(df)
+    
+    date += pd.DateOffset(hours=8)
+    #if date == pd.to_datetime('2019/10/24 2300'):
+    #    date -= pd.DateOffset(hours=1)
+
+df2.to_csv('apims-2018-2020.csv',date_format='%Y-%m-%d %H:%M')
+print("Saved! Operation complete.")
---
+++ README.md
@@ -0,0 +1,19 @@
+# api-malaysia
+
+Historical data of the Air Pollution Index (API, [wiki](https://en.wikipedia.org/wiki/Air_Pollution_Index)) in Malaysia from 2005 to 2017 with web scraping script.
+
+## Notes
+
+1. Some location may not be available such as the three mobile continuous air quality monitoring station (MCAQM) in Malaysia as its history is not saved in the website.
+2. There are some monitoring station that used to be turned on and now discontinued or vice versa.
+3. There are a few dates that the API failed to retrieve data.
+4. The web scraping script is kinda inefficent...
+5. Retrieved data is saved under the cache folder. You can unpack the archive to save some time.
+
+## Sources and Licensing
+
+Data from 2005 to 2017 are retrieved from [data.gov.my](http://www.data.gov.my/) under MOSTI, DOE and KeTSA. The data is licensed under [Creative Commons Attribution](https://opendefinition.org/licenses/cc-by/) and [Open Definition 2.1](https://opendefinition.org/od/2.1/en/).
+
+The data from 2018 to current date are retrieved directly from [APIMS](http://apims.doe.gov.my/public_v2/api_table.html) website. Data might still be under copyright (unclear) but it's *safe to assume* the data will eventually be released under data.
+
+The web scraping script is licensed under MIT.
